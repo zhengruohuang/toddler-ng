1f0fa597a6f8afe85c854e631610e820ddc84e87
Ruohuang Zheng <zhengruohuang@gmail.com>
2021-02-22 14:23:44 -0500

    Merged MIPS mem.h

--- src/arch/mips/common/include/mem.h
+++ src/arch/mips/common/include/mem.h
@@ -1,18 +1,84 @@
 #ifndef __ARCH_MIPS_COMMON_INCLUDE_MEM_H__
 #define __ARCH_MIPS_COMMON_INCLUDE_MEM_H__
 
+
 #include "common/include/abi.h"
 
-// MIPS32
+
+/*
+ * MIPS32
+ */
 #if (ARCH_WIDTH == 32)
-#include "common/include/mem32.h"
+    /*
+     * General memory management info
+     */
+    #define PAGE_SIZE           4096
+    #define PAGE_BITS           12
 
-// MIPS64
+    #define MAX_NUM_VADDR_BITS  32
+    #define MAX_NUM_PADDR_BITS  32
+
+    #define USER_VADDR_LIMIT    0x78000000ul
+
+    typedef u32                 paddr_t;
+    typedef paddr_t             ppfn_t;
+    typedef paddr_t             psize_t;
+
+    /*
+     * MIPS-specific segment
+     */
+    #define SEG_USER            0x0ul
+    #define SEG_DIRECT_CACHED   0x80000000ul
+    #define SEG_DIRECT_UNCACHED 0xa0000000ul
+    #define SEG_KERNEL          0xc0000000ul
+
+    #define SEG_USER_SIZE       0x80000000ul
+    #define SEG_DIRECT_SIZE     0x20000000ul
+    #define SEG_DIRECT_MASK     0x1ffffffful
+
+
+/*
+ * MIPS64
+ */
 #elif (ARCH_WIDTH == 64)
-#include "common/include/mem64.h"
+    /*
+     * General memory management info
+     */
+    #define PAGE_SIZE               4096
+    #define PAGE_BITS               12
 
+    #define MAX_NUM_VADDR_BITS      64
+    #define MAX_NUM_PADDR_BITS      48
+
+    #define USER_VADDR_LIMIT        0xff00000000ul
+                                    //0x3f00000000000000ul
+
+    typedef u64                     paddr_t;
+    typedef paddr_t                 ppfn_t;
+    typedef paddr_t                 psize_t;
+
+    /*
+     * MIPS-specific segment
+     */
+    #define SEG_USER                0x0ul
+    #define SEG_DIRECT_CACHED       0x9000000000000000ul
+    #define SEG_DIRECT_UNCACHED     0x9800000000000000ul
+    #define SEG_KERNEL              0xc000000000000000ul
+    #define SEG_DIRECT_CACHED_LOW   0xffffffff80000000ul
+    #define SEG_DIRECT_UNCACHED_LOW 0xffffffffa0000000ul
+
+    #define SEG_USER_SIZE           0x4000000000000000ul
+    #define SEG_DIRECT_SIZE         0x800000000000000ul
+    #define SEG_DIRECT_MASK         0x7fffffffffffffful
+    #define SEG_DIRECT_MASK_LOW     0x1ffffffful
+
+
+/*
+ * Unsupported width
+ */
 #else
-#error "Unknown arch width"
+    #error "Unsupported arch width"
 #endif
+
 
 #endif
